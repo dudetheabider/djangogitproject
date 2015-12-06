57647d9d13a7a5fe80460e806457efe1a407a823
Chris Chobar <cchobar721@gmail.com>
2015-12-06 13:45:40 -0500

    Added the models Question & Choice to polls app
    By editing the models.py file the models and attributes for the Question
    model, which will store all polling questions, and the Choice model,
    which will store all answers to polling quesitons, have been added.
    Comments describing the models have also been added.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
+# This file contains the models for the polls app
+
 from django.db import models
 
-# Create your models here.
+# Question model will be where questions and their attributes will be stored.
+# Each field below the model is esentially a column in the database
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+# The Choice model will be where the possible answers to the Questions will be stored
+# Each field below the model is esentially a column in the database
+# The question field relates each choice to one single question
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
