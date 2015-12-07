d15b8122dd787de859273c96075e73ef9c680116
Chris Chobar <cchobar721@gmail.com>
2015-12-06 19:11:27 -0500

    Added convenient viewing features to the qadmin
    By adding options to the models.py file the way published recently was
    changed. A search feature and a filter feature were added to the
    admin.py file and the question admin page.

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 # The Choice model will be where the possible answers to the Questions will be stored
 # Each field below the model is esentially a column in the database
 # The question field relates each choice to one single question
