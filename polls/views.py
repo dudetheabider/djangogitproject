ea4c4f136ac46d483372cd89b98e9b7864403cd1
Chris Chobar <cchobar721@gmail.com>
2015-12-08 19:31:45 -0500

    Fixed error in views.py and made complex detail
    Fixed an error where except was left in the detail view with no try,
    line was deleted. Using the detail.html template the detail view will
    now show the question text and choices for each question.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -9,8 +9,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-        except Question.DoesNotExist:
-        return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
