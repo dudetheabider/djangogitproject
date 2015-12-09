a472ea89dfb8acd2baac886eea792f2db3955334
Chris Chobar <cchobar721@gmail.com>
2015-12-08 21:29:22 -0500

    Fixed Syntactical Error in view.py
    Fixed syntactical error on line 14, missed parenthesis.

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -10,7 +11,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
